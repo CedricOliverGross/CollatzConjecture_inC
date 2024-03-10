f58efad2e5217997f008380bbb403f15e6955319
Cedric Gross <cedricgross@fedora.fritz.box>
2024-03-10 17:57:02 +0100

    translated variables and comments into english language

--- main.c
+++ main.c
@@ -3,30 +3,30 @@
 
 int main(int argc, char const *argv[])
 {
-    unsigned long long Anfangszahl = 1;
-    int Eins = 1;
-    unsigned long long AktuelleZahl = Anfangszahl;
-    unsigned long long Endzahl = pow(2, 64) - 1;
+    unsigned long long numberBegin = 1;
+    int one = 1;
+    unsigned long long numberCurrent = numberBegin;
+    unsigned long long numberEnd = pow(2, 64) - 1;
 
 
 
-    while (Anfangszahl != Endzahl)
+    while (numberBegin != numberEnd)    // Runs until the number where the counting starts hits the defined limit 
     {
-        printf("%20llu", AktuelleZahl); //Ausgabe der aktuellen Zahl
-        printf("%50llu\n", Anfangszahl);
+        printf("%20llu", numberCurrent); //Output of Current Number
+        printf("%50llu\n", numberBegin);
 
-        if (AktuelleZahl % 2 != 0) //Der klassiche 3x+1 Algorithmus
+        if (numberCurrent % 2 != 0) // if number is odd: multiple *3 and add 1, if not divide by 2
         {
-            AktuelleZahl = 3 * AktuelleZahl + 1;
+            numberCurrent = 3 * numberCurrent + 1;
         }else{
-            AktuelleZahl = AktuelleZahl / 2;
+            numberCurrent = numberCurrent / 2;
         }
 
-        if (Eins == AktuelleZahl)
+        if (one == numberCurrent)
         {
-            printf("%2llu\n", AktuelleZahl); //Ausgabe der aktuellen Zahl
-            Anfangszahl = Anfangszahl + 1;
-            AktuelleZahl = Anfangszahl;
+            printf("%2llu\n", numberCurrent); //Output of Current Number
+            numberBegin = numberBegin + 1;
+            numberCurrent = numberBegin;
 
         }
         
